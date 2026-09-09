9db26484a60ab8fe64fa182e18e92c0e2d7b184a
Dyad <wesadie1@gmail.com>
2026-09-09 11:12:06 +0200

    Redirect logout to public ShuffleSchool site

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -115,7 +115,7 @@ function TopNavigation() {
     if (session) {
       await supabase.auth.signOut();
       queryClient.clear();
-      window.location.href = "https://shuffle-school.onrender.com/login";
+      window.location.href = "https://shuffleschool.co.za/";
     } else {
       window.location.href = "/api/logout";
     }
